e05e2a8050599213b9335d2dfb798cafbb87bdca
sejeong00 <buttermelt@daum.net>
2023-02-27 21:35:20 +0900

    hash(), getSHA256() 함수 형태만 만듦

--- MitCommand.java
+++ MitCommand.java
@@ -12,5 +12,13 @@ public class MitCommand {
     void list(String path){
         Arrays.stream(directory.listFiles()).forEach(System.out::println);
     }
+
+    void hash(String path){
+        Arrays.stream(directory.listFiles()).forEach(i -> System.out.println(getSHA256(i)));
+    }
+
+    String getSHA256(File file){
+        return new String();
+    }
     
 }
