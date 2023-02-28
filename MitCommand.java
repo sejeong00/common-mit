fa7232258f29b3a8a7fecc11b5708679a439e8b0
sejeong00 <buttermelt@daum.net>
2023-02-28 10:10:17 +0900

    feat: getSHA256() 기능 구현

--- MitCommand.java
+++ MitCommand.java
@@ -1,4 +1,6 @@
-import java.io.File;
+import java.io.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 public class MitCommand {
@@ -17,8 +19,39 @@ public class MitCommand {
         Arrays.stream(directory.listFiles()).forEach(f -> System.out.println(getSHA256(f)));
     }
 
-    String getSHA256(File file){
-        return new String();
+    String getSHA256(File file) {
+        StringBuffer sb = new StringBuffer();
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            FileInputStream fis = new FileInputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+
+            int bufferSize = 8192;
+            byte[] buffer = new byte[bufferSize];
+            byte[] hashResult = null;
+            long read = 0;
+            long offset = file.length();
+            int unitSize;
+
+            while (read < offset) {
+                unitSize = (int) ((((offset - read)) >= bufferSize) ? bufferSize : offset - read);
+                fis.read(buffer, 0, unitSize);
+                md.update(buffer, 0, unitSize);
+                read += unitSize;
+            }
+            fis.close();
+
+            hashResult = md.digest();
+
+            for (int i = 0; i < hashResult.length; i++) {
+                sb.append(Integer.toString((hashResult[i] & 0xff) + 0x100, 16).substring(1));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return sb.toString();
     }
     
 }
